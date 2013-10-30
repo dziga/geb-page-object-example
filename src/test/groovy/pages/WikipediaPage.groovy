62c612a8845cf0e9c8eca97c7c45d67e963432c9
Nikola <0dziga0@gmail.com>
2013-10-30 02:56:43 +0100

    adding missing fields to wikipedia page

--- src/test/groovy/pages/WikipediaPage.groovy
+++ src/test/groovy/pages/WikipediaPage.groovy
@@ -3,5 +3,6 @@ package pages
 import geb.Page
 
 class WikipediaPage extends Page {
+	static url = "http://www.wikipedia.org"
 	static at = { waitFor { title == "Wikipedia, the free encyclopedia" } }
 }
